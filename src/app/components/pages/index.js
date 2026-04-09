4a0ef78e94ce980b9195a4d0711c210649d82b86
rihabnaili <nailirihab8@gmail.com>
2026-04-09 15:17:43 +0100

    update main page background

--- src/app/components/pages/index.js
+++ src/app/components/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HeroSection from '../sections/HeroSection';
-import CategoriesSection from '../sections/CategoriesSection';
 import FeaturedProducts from '../sections/FeaturedProducts';
 import ProductTabs from '../sections/ProductTabs';
 import ServicesSection from '../sections/ServicesSection';
@@ -9,7 +8,6 @@ const HomePage = () => {
   return (
     <>
       <HeroSection />
-      <CategoriesSection />
       <FeaturedProducts />
       <ServicesSection />
     </>
