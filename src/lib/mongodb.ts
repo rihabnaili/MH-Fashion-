7831594f76498dd5b69cd8bfc1dcf75bce00a0ac
MedAziz3 <hadjhassenmohamedaziz8@gmail.com>
2026-04-10 01:39:18 +0100

    test 3

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -5,7 +5,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
-
+ 
 let isConnected = false; 
 
 async function connectDB() {
