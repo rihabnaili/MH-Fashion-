807e56bf156d269503bbc1419277461f0ad60673
rihabnaili <nailirihab8@gmail.com>
2026-07-04 13:51:14 +0100

    Add Meta Pixel tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Cinzel, Montserrat, Noto_Sans_Arabic } from "next/font/google";
 import "./globals.css";
 import MainLayout from "./components/layouts/MainLayout";
 import { LanguageProvider } from "./context/LanguageContext";
 import { CartProvider } from "./context/CartContext"; 
 import { SITE_URL } from "@/lib/productRoutes";
+
+const META_PIXEL_ID = "4424903821170372";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -76,6 +79,29 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} ${cinzel.variable} ${notoSansArabic.variable}`}
       >
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         <LanguageProvider>
           <CartProvider>
             <MainLayout>{children}</MainLayout>
